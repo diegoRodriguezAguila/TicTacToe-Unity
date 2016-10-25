d7416447bef082d8c25dd43d1d3269ba382add36
diegoRodriguezAguila <diroag@gmail.com>
2016-10-25 17:34:00 -0400

    TicTacToe lesson 05 finished, Controlling the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+
+    void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("End turn not implemented!");
+    }
+}
